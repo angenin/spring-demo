e52929b087e38c4e0eaeac557a2605b7854dbaab
angenin <2643665534@qq.com>
2024-04-18 16:54:48 +0800

    配置文件-敏感数据加密

--- src/test/java/com/example/springdemo/SpringDemoApplicationTests.java
+++ src/test/java/com/example/springdemo/SpringDemoApplicationTests.java
@@ -1,14 +1,29 @@
 package com.example.springdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringDemoApplicationTests {
 
     @Test
     void contextLoads() {
         System.out.println("hello world");
     }
+
+    @Value("${password.test1}")
+    private String password1;
+    @Value("${password.test2}")
+    private String password2;
+
+    @Test
+    public void test() {
+        log.info("password1: {}", password1);
+        log.info("password2 {}", password2);
+    }
+
 
 }
