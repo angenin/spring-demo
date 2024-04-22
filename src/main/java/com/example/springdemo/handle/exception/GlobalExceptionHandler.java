a22ef2e4f1af5a542c992ea595ab6a26ab6d8a45
angenin <2643665534@qq.com>
2024-04-22 14:52:16 +0800

    异常

---
+++ src/main/java/com/example/springdemo/handle/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.example.springdemo.handle.exception;
+
+import com.example.springdemo.exception.BizException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * 全局异常处理
+ */
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 处理业务异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(BizException.class)
+    public ResponseEntity<String> handleException(BizException e) {
+        log.error("异常：", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+    /**
+     * 处理其他异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        log.error("异常：", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+}
