686b0287774948f7c4d480ad17bd46a7140cde54
angenin <2643665534@qq.com>
2024-04-26 14:57:21 +0800

    异常信息打印

--- src/main/java/com/example/springdemo/handle/GlobalExceptionHandler.java
+++ src/main/java/com/example/springdemo/handle/GlobalExceptionHandler.java
@@ -3,14 +3,23 @@ package com.example.springdemo.handle;
 import com.example.springdemo.domain.ResposeResult;
 import com.example.springdemo.enums.ResultEnum;
 import com.example.springdemo.exception.BusinessException;
+import com.example.springdemo.exception.SystemException;
 import com.example.springdemo.exception.VerifyException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
+import java.util.stream.Collectors;
 
 /**
  * 全局异常处理
@@ -20,13 +29,13 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 public class GlobalExceptionHandler {
 
     /**
-     * 处理校验异常
+     * 处理校验异常（自定义）
      * @param e
      * @return
      */
     @ExceptionHandler(VerifyException.class)
     public ResposeResult handleVerifyException(VerifyException e) {
-        log.error("校验异常：", e);
+        printlnErrorLog("校验异常", e);
         return ResposeResult.exception(e);
     }
 
@@ -37,29 +46,59 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(BusinessException.class)
     public ResposeResult handleBusinessException(BusinessException e) {
-        log.error("业务异常：", e);
+        printlnErrorLog("业务异常", e);
         return ResposeResult.exception(e);
     }
 
     /**
-     * 处理参数校验异常（@Valid注解或者@Validated进行请求参数校验）
+     * 处理系统异常
      * @param e
      * @return
      */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResposeResult handleParamException(MethodArgumentNotValidException e) {
-        log.error("参数校验异常：", e);
-        return ResposeResult.result(ResultEnum.BAD_PARAM, e.getMessage());
+    @ExceptionHandler(SystemException.class)
+    public ResposeResult handleBusinessException(SystemException e) {
+        printlnErrorLog("系统异常", e);
+        return ResposeResult.exception(e);
     }
 
     /**
-     * 处理错误的请求异常（@Valid注解或者@Validated进行请求参数校验）
+     * 参数校验异常
+     *
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class, ConstraintViolationException.class,
+            BindException.class, MissingServletRequestParameterException.class})
+    public ResposeResult handleValidException(Exception e) {
+        printlnErrorLog("参数校验异常", e);
+        String errorMsg = "";
+        if (e instanceof MethodArgumentNotValidException) {
+            errorMsg = ((MethodArgumentNotValidException) e).getBindingResult().getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage).collect(Collectors.joining("; "));
+
+        } else if (e instanceof ValidationException) {
+            errorMsg = ((ConstraintViolationException) e).getConstraintViolations().stream()
+                    .map(ConstraintViolation::getMessage).collect(Collectors.joining("; "));
+
+        } else if (e instanceof BindException) {
+            errorMsg = ((BindException) e).getAllErrors().stream().map(ObjectError::getDefaultMessage)
+                    .collect(Collectors.joining("; "));
+
+        } else if (e instanceof MissingServletRequestParameterException) {
+            errorMsg = ((MissingServletRequestParameterException) e).getParameterName();
+        }
+
+        return ResposeResult.exception(new VerifyException(ResultEnum.BAD_PARAM, errorMsg));
+    }
+
+    /**
+     * 处理错误的请求异常
      * @param e
      * @return
      */
     @ExceptionHandler({HttpMessageNotReadableException.class, HttpRequestMethodNotSupportedException.class})
     public ResposeResult handleRequestException(Exception e) {
-        log.error("错误的请求异常：", e);
+        printlnErrorLog("错误的请求异常", e);
         return ResposeResult.result(ResultEnum.BAD_REQUEST);
     }
 
@@ -70,7 +109,7 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResposeResult handle404Exception(NoHandlerFoundException e) {
-        log.error("404异常：", e);
+        printlnErrorLog("404异常", e);
         return ResposeResult.result(ResultEnum.NOT_FOUND);
     }
 
@@ -81,8 +120,28 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(Exception.class)
     public ResposeResult handleException(Exception e) {
-        log.error("系统异常：", e);
+        printlnErrorLog("系统异常", e);
         return ResposeResult.result(ResultEnum.SYS_ERROR);
+    }
+
+    /**
+     * 异常日志
+     * @param title
+     * @param e
+     */
+    private void printlnErrorLog(String title, Exception e) {
+        StackTraceElement stackTrace = e.getStackTrace()[0];
+        log.error("\n----------------------- exception info -----------------------\n\t" +
+                        "exception title: {}\n\t" +
+                        "exception type: {}\n\t" +
+                        "exception message: {}\n\t" +
+                        "file name: {}\n\t" +
+                        "class name: {}\n\t" +
+                        "method name: {}\n\t" +
+                        "line number: {}\n\t" +
+                        "full exception info: ",
+                title, e.getClass().getName(), e.getMessage(), stackTrace.getFileName(), stackTrace.getClassName(),
+                stackTrace.getMethodName(), stackTrace.getLineNumber(), e);
     }
 
 }
